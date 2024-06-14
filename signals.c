925b5c19aec5af91f702ca6c64c3e97566780f25
Vinicius Vaccari Hehn <vivaccar@c1r9s2.42porto.com>
2024-06-14 11:01:23 +0100

    configured signals

--- signals.c
+++ signals.c
@@ -6,7 +6,7 @@
 /*   By: vivaccar <vivaccar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 19:19:10 by vivaccar          #+#    #+#             */
-/*   Updated: 2024/06/14 10:40:15 by vivaccar         ###   ########.fr       */
+/*   Updated: 2024/06/14 10:57:35 by vivaccar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	signal_handler(int signal, siginfo_t *info, void *content)
 	(void)info;
 	if (signal == SIGINT)
 	{
-		write(1, "\n", 1);
+		ft_printf(1, "\n");
 		rl_on_new_line();
 		rl_replace_line("", 0);
 		rl_redisplay();
@@ -32,12 +32,14 @@ void	start_sigaction()
 
 	sigemptyset(&sa_quit.sa_mask);
 	sigemptyset(&sa_int.sa_mask);
-	sa_int.sa_sigaction = &signal_handler;
+	sa_quit.sa_flags = 0;
+	sa_int.sa_flags = 0;
 	sa_quit.sa_handler = SIG_IGN;
-	sigaddset(&sa_int.sa_mask, SIGINT);
+	sa_int.sa_sigaction = &signal_handler;
 	sigaddset(&sa_quit.sa_mask, SIGQUIT);
-	sigaddset(&sa_int.sa_mask, SIGQUIT);
 	sigaddset(&sa_quit.sa_mask, SIGINT);
-	sigaction(SIGINT, &sa_int, NULL);
+	sigaddset(&sa_int.sa_mask, SIGQUIT);
+	sigaddset(&sa_int.sa_mask, SIGINT);
 	sigaction(SIGQUIT, &sa_quit, NULL);
+	sigaction(SIGINT, &sa_int, NULL);
 }
