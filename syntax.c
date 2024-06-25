2fc5d93a1f27a5b305a1ea1c67e7f9ab719f61d5
Marcelo Fassbinder Martins <marcelobinder9@gmail.com>
2024-06-25 15:24:13 +0100

    tokens are prepared

--- syntax.c
+++ syntax.c
@@ -6,81 +6,63 @@
 /*   By: vivaccar <vivaccar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 11:31:30 by vivaccar          #+#    #+#             */
-/*   Updated: 2024/06/24 18:42:14 by vivaccar         ###   ########.fr       */
+/*   Updated: 2024/06/25 14:48:20 by vivaccar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/minishell.h"
 
-bool	unclosed_quotes(char *str)
+bool	check_redir(char *str)
 {
-	bool	flag;
-	char	to_find;
+	char	redir_type;
 
-	flag = false;
-	while (*str)
+	redir_type = *str;
+	str++;
+	if (*str == redir_type)
+		str++;
+	if (exceeded_token(str, redir_type) || empty_line(str))
 	{
-		if (*str == 34 || *str == 39)
-		{
-			to_find = *str;
-			str++;
-			flag = true;
-			while (*str && *str != to_find)
-				str++;
-			if (*str == to_find)
-				flag = false;
-		}
-		if (*str)
-			str++;
+		ft_printf(STDERR_FILENO,
+		"minishell: syntax error unexpected token '%c'\n",
+		redir_type);
+		return (true);
 	}
-	if (flag == true)
-		ft_printf (STDERR_FILENO, "minishell: syntax error unclosed quotes\n");
-	return (flag);
+	return (false);
 }
 
-bool	empty_line(char *str)
+bool	check_pipe(char *str)
 {
-	while (*str)
+	str++;
+	if (empty_line(str) || exceeded_token(str, '|') || previous_is_redir(str))
 	{
-		if (!(*str >= 9 || *str <= 13) || *str != 32)
-			return (false);
-		str++;
-	}
-	return (true);
-}
-
-bool	exceeded_token(char *str, int c)
-{
-	while (*str)
-	{
-		if (*str == c)
-			return (true);
-		if (!(*str >= 9 || *str <= 13) || *str != 32)
-			break ;
-		str++;
+		ft_printf(STDERR_FILENO,
+		"minishell: syntax error unexpected token |\n");
+		return (true);
 	}
 	return (false);
 }
 
 bool	redir_error(char *str)
 {
-	char	redir_type;
+	enum 	e_status	status;	
 
+	status = GENERAL;
 	while (*str)
 	{
-		if (*str == '>' || *str == '<')
+		if (*str == D_QTE && status == GENERAL)
+			status = IN_D_QUOTE;
+		else if (*str == S_QTE && status == GENERAL)
+			status = IN_S_QUOTE;
+		else if (*str == S_QTE && status == IN_S_QUOTE)
+			status = GENERAL;
+		else if (*str == D_QTE && status == IN_D_QUOTE)
+			status = GENERAL;
+		if ((*str == '>' || *str == '<') && (status == GENERAL))
 		{
-			redir_type = *str;
-			str++;
-			if (*str == redir_type)
-				str++;
-			if (exceeded_token(str, redir_type) || empty_line(str))
-			{
-				ft_printf(STDERR_FILENO,
-					"minishell: syntax error unexpected token '%c'\n",
-					redir_type);
+			if (check_redir(str))
 				return (true);
-			}
+			else
+				str++;
 		}
 		else
 			str++;
@@ -88,35 +70,25 @@ bool	redir_error(char *str)
 	return (false);
 }
 
-bool	previous_is_redir(char *str)
-{
-	str = str - 2;
-	if (*str == '>' && *(str - 1) != '>')
-		return (false);
-	while (str)
-	{
-		if (*str == '>')
-			return (true);
-		else if ((*str  < 9 || *str > 13) && *str != 32)
-			return (false);
-		str--;
-	}
-	return (false);
-}
-
 bool	pipe_error(char *str)
 {
+	enum e_status	status;
+
+	status = GENERAL;
 	while (*str)
 	{
-		if (*str == '|')
+		if (*str == D_QTE && status == GENERAL)
+			status = IN_D_QUOTE;
+		else if (*str == S_QTE && status == GENERAL)
+			status = IN_S_QUOTE;
+		else if (*str == S_QTE && status == IN_S_QUOTE)
+			status = GENERAL;
+		else if (*str == D_QTE && status == IN_D_QUOTE)
+			status = GENERAL;
+		if (*str == '|' && status == GENERAL)
 		{
-			str++;
-			if (empty_line(str) || exceeded_token(str, '|') || previous_is_redir(str))
-			{
-				ft_printf(STDERR_FILENO,
-					"minishell: syntax error unexpected token |\n");
+			if (check_pipe(str))
 				return (true);
-			}
 		}
 		str++;
 	}
