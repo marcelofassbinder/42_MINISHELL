29dd76b97bcd2a15cb371ca0051ff9ea644c011a
Marcelo Fassbinder Martins <marcelobinder9@gmail.com>
2024-07-08 20:40:07 +0100

    deleted check_cd_args function

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,32 +6,21 @@
 /*   By: mfassbin <mfassbin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/06 16:36:03 by mfassbin          #+#    #+#             */
-/*   Updated: 2024/07/08 18:56:29 by mfassbin         ###   ########.fr       */
+/*   Updated: 2024/07/08 20:39:04 by mfassbin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-
-int	check_cd_args(char **cmd_args, t_shell *shell)
-{
-	if (cmd_args[1])
-	{
-		if (cmd_args[2])
-		{
-			safe_chdir(NULL, shell, 2);
-			return (0);
-		}
-	}
-	return (1);
-}	
-
 void	cd(char **cmd_args, t_shell *shell)
 {
 	char *update_old_pwd;
 
-	if (!check_cd_args(cmd_args, shell))
-		return ;
+	if (cmd_args[1])
+	{
+		if (cmd_args[2])
+			safe_chdir(NULL, shell, 2);
+	}
 	update_old_pwd = safe_getcwd(NULL, 0, shell);
 	if (!cmd_args[1] || !ft_strncmp(cmd_args[1], "--", 3)
 		|| !ft_strncmp(cmd_args[1], "~", 2))
