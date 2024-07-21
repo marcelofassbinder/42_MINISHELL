67e2b001b28c1eaefbf1bd1d91611c86aba00ce8
Marcelo Fassbinder Martins <marcelobinder9@gmail.com>
2024-07-21 17:38:46 +0100

    expansion heredoc finished

--- heredoc/expand_heredoc.c
+++ heredoc/expand_heredoc.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   heredoc.c                                          :+:      :+:    :+:   */
+/*   expand_heredoc.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mfassbin <mfassbin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/20 15:13:32 by mfassbin          #+#    #+#             */
-/*   Updated: 2024/07/21 15:46:39 by mfassbin         ###   ########.fr       */
+/*   Updated: 2024/07/21 17:32:18 by mfassbin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,14 @@
 char	*expand_here_doc(char *line, t_shell *shell)
 {
 	char *after_doll;
-	char *to_expand;
 	char *to_free;
 	char *copy_line;
-	char *expanded;
-	int	flag;
 	int i;
 
 	copy_line = ft_strdup(line);
 	to_free = copy_line;
 	while (ft_strchr(copy_line, DOLLAR))
 	{
-		flag = 0;
 		i = 0;
 		while(*copy_line != DOLLAR)
 			copy_line++;
@@ -34,35 +30,23 @@ char	*expand_here_doc(char *line, t_shell *shell)
 		while(copy_line[i] != ' ' && copy_line[i] != '\n' && copy_line[i] != S_QTE  && copy_line[i] != D_QTE)
 			i++;
 		if (i == 0)
-			line = replace_expanded_var(line, to_expand, expanded, shell, 1);	
-		else
-		{
-			after_doll = ft_substr(copy_line, 0, i);
-			expanded = NULL;
-			to_expand = ft_strdup(after_doll);
-			expanded = expand(to_expand, shell);
-			line = replace_expanded_var(line, after_doll, expanded, shell, 0);
-		}
+			continue ;
+		after_doll = ft_substr(copy_line, 0, i);
+		line = replace_expanded_var(line, after_doll, shell);
 	}
 	free(to_free);
 	return(line);
 }
 
-char	*replace_expanded_var(char *line, char *after_doll, char *expanded, t_shell *shell, int flag)
+char	*replace_expanded_var(char *line, char *after_doll, t_shell *shell)
 {
 	char *new_line;
-	int	len;
+	char *to_expand;
+	char *expanded;
 
-	if (flag == 1)
-		return(line);
-	if (expanded)
-		len = ft_strlen(line) + ft_strlen(expanded) - ft_strlen(after_doll) + 2;
-	else
-		len = ft_strlen(line) - ft_strlen(after_doll) + 2;
-	new_line = ft_calloc(sizeof(char), len);
-	if (!new_line)
-		shell_error(shell, "Calloc Error: here_doc expansion", 0, true);
-	new_line = copy_before_doll(line, after_doll, expanded, shell);
+	to_expand = ft_strdup(after_doll);
+	expanded = expand(to_expand, shell);
+	new_line = copy_before_doll(line, after_doll, shell);
 	if (expanded)
 		new_line = ft_strjoin(new_line, expanded);		
 	new_line = copy_after_doll(new_line, line, after_doll);
@@ -78,8 +62,8 @@ char *copy_after_doll(char *new_line, char *line, char *after_doll)
 	int i;
 	int copy;
 
-	i = 0;
-	while(line[i])
+	i = -1;
+	while(line[++i])
 	{
 		if (line[i] == DOLLAR && line[i + 1] == after_doll[0])
 		{
@@ -93,25 +77,22 @@ char *copy_after_doll(char *new_line, char *line, char *after_doll)
 			if (j == ft_strlen(after_doll))
 				copy = i;
 		}
-		i++;
 	}
 	new_line = ft_strjoin(new_line, &line[copy]);
 	return(new_line);
 }
 
-char *copy_before_doll(char *line, char *after_doll, char *expanded, t_shell *shell)
+char *copy_before_doll(char *line, char *after_doll, t_shell *shell)
 {
 	char	*new;
 	int		i;
 	int		j;
-	int		copy;
 
-	copy = 0;
 	new = ft_calloc(sizeof(char), ft_strlen(line) - ft_strlen(after_doll));
 	if (!new)
 		shell_error(shell, "Calloc Error : here_doc expansion", 0, true);
-	i = 0;
-	while(line[i])
+	i = -1;
+	while(line[++i])
 	{
 		if (line[i] == DOLLAR && line[i + 1] == after_doll[0])
 		{
@@ -123,20 +104,9 @@ char *copy_before_doll(char *line, char *after_doll, char *expanded, t_shell *sh
 				j++;
 			}
 			if (j == ft_strlen(after_doll))
-				copy = i - j;
+				j = i - j;
 		}
-		i++;
 	}
-	i = 0;
-	if (!expanded || !ft_strcmp(expanded, ""))
-		copy--;
-	while(i < copy)
-	{
-		if (line[i] == DOLLAR && line[i + 1] != ' ' && line[i + 1] != '\n')
-			i++;
-		new[i] = line[i];
-		i++;
-	}
-	new[i] = '\0';
+	ft_memcpy(new, line, j - 1);
 	return (new);
 }
---
+++ heredoc/run_heredoc.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   run_heredoc.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfassbin <mfassbin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/21 16:24:11 by mfassbin          #+#    #+#             */
+/*   Updated: 2024/07/21 16:24:52 by mfassbin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+char	*write_here_doc(t_redir *redir, t_shell *shell)
+{
+	char *buffer;
+	char *line;
+
+	buffer = ft_calloc(sizeof(char), 1);
+	if (!buffer)
+		shell_error(shell, "Calloc Error: here_doc", 0, true);
+	dup2(STDERR_FILENO, STDIN_FILENO);
+	while(1)
+	{
+		line = readline(">");
+		line = add_backslash_n(line, shell);
+		if (redir->file_status == GENERAL)
+			line = expand_here_doc(line, shell);
+		if (!ft_strncmp(line, redir->file, ft_strlen(redir->file)) && ft_strlen(line) == ft_strlen(redir->file) + 1)
+		{
+			free(line);
+			break ;
+		}
+		buffer = ft_strjoin(buffer, line);
+		free(line);
+	}
+	return (buffer);
+}
+
+char	*add_backslash_n(char *line, t_shell *shell)
+{
+	char	*new_line;
+	int 	len;
+
+	len = ft_strlen(line);
+	new_line = ft_calloc(sizeof(char), ft_strlen(line) + 2);
+	if (!new_line)
+		shell_error(shell, "Calloc Error: here_doc expansion", 0, true);
+	ft_memcpy(new_line, line, len);
+	new_line[len] = '\n';
+	free(line);
+	return(new_line);
+}
+
+int	run_here_doc(t_redir *redir, t_shell *shell)
+{
+	char *buffer;
+	int fd[2];
+
+	buffer = write_here_doc(redir, shell);
+	if (pipe(fd) == -1)
+		shell_error(shell, "Pipe error\n", 0, true);	
+	if (safe_fork(shell) == 0)
+	{
+		close(fd[0]);
+		write(fd[1], buffer, ft_strlen(buffer));
+		free(buffer);
+		dup2(fd[1], shell->fd_out);
+		close(fd[1]);
+		//get_next_line(-1);
+		free_and_exit(shell);
+	}
+	wait(NULL);
+	//get_next_line(-1);
+	free(buffer);
+	close(fd[1]);
+	add_here_doc_fd(shell, fd[0], redir->id, false);
+	return (1);
+}
+
+void	add_here_doc_fd(t_shell *shell, int fd_here_doc, int pos, bool init)
+{
+	if (init)
+	{
+		shell->fd_heredoc = ft_calloc(sizeof(int), shell->count_hd + 1);
+		if (!shell->fd_heredoc)
+			shell_error(shell, "Calloc error: heredoc", 0, true);
+		shell->fd_heredoc[shell->count_hd] = -1;
+		return ;
+	}
+	shell->fd_heredoc[pos] = fd_here_doc;
+}
